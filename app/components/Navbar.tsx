1881ca4fb17e3b6d2f3680b5241bf358660099ac
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-07-31 23:06:31 +0200

    feat: setup initial environment

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "@/public/logo.png";
+import { auth, signOut, signIn } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans ">
+      <nav className="flex justify-between items-center">
+        <Link className="text-black" href="/">
+          <Image src={logo} alt="logo" />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span>signout</span>
+                </button>
+              </form>
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github", { redirectTo: "/" });
+              }}
+            >
+              <button type="submit" className="hover:cursor-pointer">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
