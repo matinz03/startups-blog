c7a8f33d5c9d83bbb5826c40f0af860ad25d944f
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-08-01 16:07:47 +0200

    feat: install sanity and finish card section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,60 @@
 import React from "react";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 const Home = async ({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) => {
+  const posts = [
+    {
+      _createdAt: new Date(),
+
+      views: 55,
+
+      author: { _id: 1, name: "Matinz03" },
+
+      _id: 1,
+
+      description: "This is a description",
+
+      image:
+        "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0 3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+      category: "Robots",
+
+      title: "We Robots",
+    },
+  ];
   const query = (await searchParams).query;
   return (
-    <section className="pink_container">
-      <h1 className="heading">
-        Pitch Your Startup,
-        <br /> Connect With Entrepreneurs
-      </h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Pitches, and Get Noticed
-      </p>
-      <SearchForm query={query} />
-    </section>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+      </section>
+      <ul className="mt-7 card_grid">
+        {posts?.length > 0 ? (
+          posts.map((StartupCardType) => (
+            <StartupCard post={StartupCardType} key={StartupCardType._id} />
+          ))
+        ) : (
+          <p className="no-results">No startups found</p>
+        )}
+      </ul>
+    </>
   );
 };
 
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [{ protocol: "https", hostname: "*" }],
+  },
   /* config options here */
 };
 
