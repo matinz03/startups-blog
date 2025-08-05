2e178ca15bfdcf3a58ec9c734379a82f9941cd54
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-08-05 22:43:03 +0200

    feat: add playlist and parallel fetching

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,31 +1,48 @@
+import { Suspense } from "react";
+import { client } from "@/sanity/lib/client";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
-import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import { formatDate } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+
 export const experimental_ppr = true;
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const search = { id: id || null };
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, search);
+
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
+
   if (!post) return notFound();
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
-        <p className=" tag">{formatDate(post?._createdAt)}</p>
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
+
       <section className="section_container">
         <img
           src={post.image}
           alt="thumbnail"
-          className=" h-auto mx-auto rounded-xl"
+          className="w-full h-auto rounded-xl"
         />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
@@ -33,15 +50,24 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
               className="flex gap-2 items-center mb-3"
             >
               <Image
-                src={post.author?.image}
+                src={post.author.image}
                 alt="avatar"
-                height={64}
                 width={64}
+                height={64}
                 className="rounded-full drop-shadow-lg"
               />
+
+              <div>
+                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post.author.username}
+                </p>
+              </div>
             </Link>
+
             <p className="category-tag">{post.category}</p>
           </div>
+
           <h3 className="text-30-bold">Pitch Details</h3>
           {post.pitch ? (
             <article
@@ -52,14 +78,27 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
             <p className="no-result">no pitch ideas provided</p>
           )}
         </div>
-        <hr className="divider" />
-      </section>
 
-      <Suspense fallback={<Skeleton className="view_skeleton" />}>
-        <View id={id} />
-      </Suspense>
+        <hr className="divider" />
+
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
+        </Suspense>
+      </section>
     </>
   );
 };
 
-export default page;
+export default Page;
