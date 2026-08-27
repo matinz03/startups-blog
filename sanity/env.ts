0cc37bde5ca0daae4c8889147cf929636d235762
Matin <matinpsn1@gmail.com>
2026-08-27 21:13:31 +0200

    Add fallback values in sanity env for build phase

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,16 +1,13 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-08-01";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
-);
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
-);
-export const token = process.env.SANITY_WRITE_TOKEN;
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "demo";
+export const token = process.env.SANITY_WRITE_TOKEN || "";
+
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
